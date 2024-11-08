a1012f2b6c3c4789b2c11af8f2c8b3c472dd5679
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-08 07:35:32 -0500

    The second label was created

--- UIKit4_UILabelUsingCode/ViewController.swift
+++ UIKit4_UILabelUsingCode/ViewController.swift
@@ -19,15 +19,33 @@ class ViewController: UIViewController {
         
         return label
     }()
+    
+    private let swiftBetaLabel2: UILabel = {
+        let label = UILabel()
+        label.text = "Learn to program apps in Swift and SwiftUI, create apps on Xcode and upload to App Store"
+        label.font = UIFont(name: "Arial Rounded MT Bold", size: 12)
+        label.textAlignment = .left
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(swiftBetaLabel1)
+        view.addSubview(swiftBetaLabel2)
         
         NSLayoutConstraint.activate([
             swiftBetaLabel1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            swiftBetaLabel1.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+            swiftBetaLabel1.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            
+            swiftBetaLabel2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            swiftBetaLabel2.topAnchor.constraint(equalTo: swiftBetaLabel1.bottomAnchor, constant: 12),
+            swiftBetaLabel2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            swiftBetaLabel2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
         ])
     }
 
