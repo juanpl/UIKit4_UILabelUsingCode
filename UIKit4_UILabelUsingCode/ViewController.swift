d8b0cae2fd7ba8baead7615ed1b2cf30e3274218
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-08 17:16:42 -0500

    The third label was created. A dictionary was created to implement the label attributes

--- UIKit4_UILabelUsingCode/ViewController.swift
+++ UIKit4_UILabelUsingCode/ViewController.swift
@@ -31,12 +31,34 @@ class ViewController: UIViewController {
         
         return label
     }()
+    
+    private let swiftBetaLabel3: UILabel = {
+        
+        let text = "Learn to program apps in Swift and SwiftUI, create apps on Xcode and upload to App Store"
+        
+        let attributedText : [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+            .underlineColor: UIColor.red,
+            .foregroundColor: UIColor.white,
+            .backgroundColor: UIColor.blue,
+            .font: UIFont.systemFont(ofSize: 32)
+        ]
+        let attributedString = NSAttributedString(string: text, attributes: attributedText)
+        
+        let label = UILabel()
+        label.attributedText = attributedString
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(swiftBetaLabel1)
         view.addSubview(swiftBetaLabel2)
+        view.addSubview(swiftBetaLabel3)
         
         NSLayoutConstraint.activate([
             swiftBetaLabel1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -45,7 +67,12 @@ class ViewController: UIViewController {
             swiftBetaLabel2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             swiftBetaLabel2.topAnchor.constraint(equalTo: swiftBetaLabel1.bottomAnchor, constant: 12),
             swiftBetaLabel2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
-            swiftBetaLabel2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
+            swiftBetaLabel2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            
+            swiftBetaLabel3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            swiftBetaLabel3.topAnchor.constraint(equalTo: swiftBetaLabel2.bottomAnchor, constant: 12),
+            swiftBetaLabel3.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            swiftBetaLabel3.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
         ])
     }
 
